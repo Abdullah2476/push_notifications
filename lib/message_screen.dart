1fce9eb2913c65da9f155756a0d0713c30fdf907
Abdullah2476 <abdullahrajpoot2476@gmail.com>
2025-12-03 20:07:03 +0500

    comments made for readability

--- lib/message_screen.dart
+++ lib/message_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+//message screen where message is to be displayed or further chating or something else done
+
 class MessageScreen extends StatelessWidget {
   const MessageScreen({super.key});
 
